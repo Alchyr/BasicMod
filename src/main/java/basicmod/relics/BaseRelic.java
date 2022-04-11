d69fc52e4c1ccde609e6138a9849564bbf583759
Alchyr <isithalchyr@gmail.com>
2022-04-11 11:23:01 -0700

    files in folders so that git tracks them

--- src/main/java/basicmod/relics/BaseRelic.java
+++ src/main/java/basicmod/relics/BaseRelic.java
@@ -2,12 +2,24 @@ package basicmod.relics;
 
 import basemod.abstracts.CustomRelic;
 import basicmod.util.TextureLoader;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
 
 import static basicmod.BasicMod.relicPath;
 
 public abstract class BaseRelic extends CustomRelic {
-    public BaseRelic(String id, String textureName, RelicTier tier, LandingSound sfx) {
-        super(id, TextureLoader.getTexture(relicPath(textureName + ".png")), tier, sfx);
-        outlineImg = TextureLoader.getTextureNull(relicPath(textureName + "Outline.png"));
+    protected String imageName;
+
+    public BaseRelic(String id, String imageName, RelicTier tier, LandingSound sfx) {
+        super(id, TextureLoader.getTexture(relicPath(imageName + ".png")), tier, sfx);
+
+        this.imageName = imageName;
+        outlineImg = TextureLoader.getTextureNull(relicPath(imageName + "Outline.png"));
+    }
+
+    @Override
+    public void loadLargeImg() {
+        if (largeImg == null) {
+            this.largeImg = ImageMaster.loadImage(relicPath("large/" + imageName + ".png"));
+        }
     }
 }
